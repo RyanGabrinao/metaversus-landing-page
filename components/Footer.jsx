f80fb777f62ec7cecebd4bf9cab43806ed852840
Ryan Builds <ryan.gabrinao@gmail.com>
2023-04-10 21:33:43 -0700

    fixed year text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => (
         <div className="flex flex-wrap items-center justify-between gap-4">
           <h4 className="font-extrabold text-[24px] text-white">METAVERUS</h4>
           <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+            Copyright © 2021 - 2023 Metaversus. All rights reserved.
           </p>
 
           <div className="flex gap-4">
