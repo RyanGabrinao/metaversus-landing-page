c6b396cb164d0855f2d50da77a898f59255a3934
Ryan Builds <ryan.gabrinao@gmail.com>
2023-04-10 21:19:31 -0700

    initial commit

---
+++ sections/Explore.jsx
@@ -0,0 +1,47 @@
+"use client";
+import { useState } from "react";
+import { motion as m } from "framer-motion";
+
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { TypingText, ExploreCard, TitleText } from "../components";
+import { exploreWorlds } from "../constants";
+
+const Explore = () => {
+  const [active, setActive] = useState("world-2");
+  return (
+    <section className={`${styles.paddings}`} id="explore">
+      <m.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText
+          title={
+            <>
+              Choose the world you want
+              <br className="hidden md:block" /> to explore
+            </>
+          }
+          textStyles="text-center"
+        />
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+          {exploreWorlds.map((world, idx) => (
+            <ExploreCard
+              key={world.id}
+              {...world}
+              index={idx}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </m.div>
+    </section>
+  );
+};
+
+export default Explore;
